1473580cc392ad5ea636a2c4cc7b5f69db7b2f87
polysaturate <rob.race@me.com>
2016-01-22 15:17:12 -0700

    Update bitbucket.rb

--- lib/omniauth/strategies/bitbucket.rb
+++ lib/omniauth/strategies/bitbucket.rb
@@ -34,6 +34,10 @@ module OmniAuth
                         ri
                       end
       end
+      
+      def callback_url
+        options[:redirect_uri] || (full_host + script_name + callback_path)
+      end
     end
   end
 end
